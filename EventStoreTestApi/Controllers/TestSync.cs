6fe347d006c460370e13551ef116d4a6ecc98842
Ben Foster <ben.foster@checkout.com>
2017-08-07 19:19:13 +0100

    Added sync endpoint

---
+++ EventStoreTestApi/Controllers/TestSync.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Web.Http;
+using EventStore.ClientAPI;
+using SerilogTimings.Extensions;
+
+namespace EventStoreTestApi.Controllers
+{
+    public class TestSyncController : ApiController
+    {
+        public IHttpActionResult Post()
+        {
+            WriteAuthoriseEventsSync();
+            return Ok();
+        }
+
+        private static void WriteAuthoriseEventsSync()
+        {
+            var requestedEvent = new EventData(Guid.NewGuid(), "ChargeRequested", true,
+                Encoding.UTF8.GetBytes(JsonEvents.RequestedEvent), null);
+
+            var authorisedEvent = new EventData(Guid.NewGuid(), "ChargeAuthorised", true,
+                Encoding.UTF8.GetBytes(JsonEvents.AuthorisedJson), null);
+
+            var chargeId = Guid.NewGuid();
+
+            using (Global.Logger.TimeOperation("Writing to event store"))
+            {
+                Global.EsConnection.AppendToStreamAsync(
+                    $"Test.CardCharge-{chargeId}",
+                    ExpectedVersion.Any, requestedEvent,
+                    authorisedEvent).Wait();
+            }
+        }
+
+    }
+}
